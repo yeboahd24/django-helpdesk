bd3908bc39fcbc27e8580e27698e8ba5407976f8
Dominic <65431569+yeboahd24@users.noreply.github.com>
2023-03-29 10:46:47 +0100

    Update settings.py

--- demo/demodesk/config/settings.py
+++ demo/demodesk/config/settings.py
@@ -197,8 +197,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "dominic@panabios.org"
-EMAIL_HOST_PASSWORD = "atiigbchasogaaxi"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
